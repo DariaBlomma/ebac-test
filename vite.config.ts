5599f2d4fbfd7a2804bba63b426d1ae1b11a8588
Дарья Козина <kozina.flowers@yandex.ru>
2023-01-28 21:57:45 +0300

    added deploy to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
-import { fileURLToPath, URL } from 'node:url';
+import {
+  fileURLToPath,
+  URL,
+} from 'node:url';
 
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
+const isProduction = process.env.APP_ENV === 'production';
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: isProduction ? '/ebac-test/' : '/',
   plugins: [vue()],
   resolve: {
     alias: {
